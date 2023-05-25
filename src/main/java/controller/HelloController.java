089c92025bec1055102d0c169771f5fbfc5df9c8
Gilberth Chaves A <gchavesav@gmail.com>
2023-05-24 19:12:51 -0600

    Metodos del profesor HelloController

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,14 +1,57 @@
-package ucr.lab;
+package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import ucr.lab.HelloApplication;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Text txtMessage;
+
+    private void loadPage(String page) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(page));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void Exit(ActionEvent event) {
+        System.exit(0);
     }
+
+    @FXML
+    void Home(ActionEvent event) {
+        this.txtMessage.setText("Laboratory No. 9");
+        this.bp.setCenter(ap);
+    }
+
+    @FXML
+    void bTreeOperationsOnAction(ActionEvent event) {
+        loadPage("bTreeOperations.fxml");
+    }
+
+    @FXML
+    void bTreeTourOnAction(ActionEvent event) {
+        loadPage("transversalTour.fxml");
+    }
+
+    @FXML
+    void graphicBTreeOnAction(ActionEvent event) {
+        loadPage("graphicBTree.fxml");
+    }
+
 }
