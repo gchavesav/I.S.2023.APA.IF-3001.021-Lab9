3ad425584124d0a8454965d3662d31a682ac7cf1
gabriel.perez <79992458+gabrielperezpacheco@users.noreply.github.com>
2023-05-24 21:06:39 -0600

    -Sin terminar no subir

--- src/main/java/controller/GraphicBTreeController.java
+++ src/main/java/controller/GraphicBTreeController.java
@@ -9,8 +9,10 @@ import javafx.scene.Group;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import ucr.lab.HelloApplication;
 
@@ -32,6 +34,8 @@ public class GraphicBTreeController
     @javafx.fxml.FXML
     private Text postOrderTextField;
     private BTree tree;
+    @javafx.fxml.FXML
+    private Pane lienzo;
 
     @javafx.fxml.FXML
     public void initialize() throws TreeException {
@@ -39,32 +43,43 @@ public class GraphicBTreeController
         for (int i = 0; i < 10; i++) {
             tree.add(util.Utility.random(50));
         }
-        drawBinaryTree(tree.getRoot(), 0, 0, 0);
+        drawBinaryTree(tree.getRoot(),  400, 50, 200);
         setFields();
     }
 
     private void drawBinaryTree(BTreeNode node, double x, double y, int level) {
         if (node != null) {
-            // Create and position a visual representation of the current node
-            nodeCircle = new Circle(20);
-            nodeCircle.setStroke(Color.WHITE);
-            nodeCircle.setStroke(javafx.scene.paint.Color.WHITE);
-            nodeCircle.setCenterX(x);
-            nodeCircle.setCenterY(y);
+            // Dibujar las conexiones con los nodos hijos primero
+            if (node.left != null) {
+                double childX = x - level / 2;
+                double childY = y + 50;
+                drawBinaryTree(node.left, childX, childY, level / 2);
 
-            // Add any necessary styling or labels to the nodeCircle
+                // Dibujar una línea desde el nodo actual al nodo hijo izquierdo
+                Line line = new Line(x, y, childX, childY - 20);
+                lienzo.getChildren().add(line);
+            }
 
-            // Add the nodeCircle to the treeGroup
-            treeGroup.getChildren().add(nodeCircle);
+            if (node.right != null) {
+                double childX = x + level / 2;
+                double childY = y + 50;
+                drawBinaryTree(node.right, childX, childY, level / 2);
 
-            // Calculate the coordinates for the left and right child nodes
-            double childY = y + 100;
-            double leftChildX = x - Math.pow(2, level) * 30;
-            double rightChildX = x + Math.pow(2, level) * 30;
+                // Dibujar una línea desde el nodo actual al nodo hijo derecho
+                Line line = new Line(x, y, childX, childY - 20);
+                lienzo.getChildren().add(line);
+            }
 
-            // Recursively draw the left and right child nodes
-            drawBinaryTree(node.left, leftChildX, childY, level + 1);
-            drawBinaryTree(node.right, rightChildX, childY, level + 1);
+            // Dibujar el nodo actual como un círculo después de las líneas
+            Circle circle = new Circle(x, y, 20);
+            circle.setFill(Color.LIGHTGREEN); // Cambiar el color a verde claro
+            lienzo.getChildren().add(circle);
+
+            // Mostrar el valor del nodo
+            Text text = new Text(String.valueOf(node.data));
+            text.setX(x - 5);
+            text.setY(y + 5);
+            lienzo.getChildren().add(text);
         }
     }
 
